885e60a023cb17f2dd4c315c5acfb96498ed101f
chang-ha <kym637@naver.com>
2023-04-13 21:22:12 +0900

    우창하 : 2023-04-13

---
+++ PersonalStudy/040강. 구조체 포인터/040강. 구조체 포인터.cpp
@@ -0,0 +1,28 @@
+﻿// 040강. 구조체 포인터
+
+
+
+#include <iostream>
+typedef struct _tagMyst
+{
+	// "구조체 안에 있는것은 변수가 아닌 멤버!!!"
+	int a; // 4바이트
+	float f; // 4바이트
+}MYST;
+int main()
+{
+	// 구조체 포인터
+	MYST s;
+
+	// 변수
+	int a = 0;
+	// 변수(구조체)
+	s;
+	// 변수의 멤버(int 파트)
+	s.a;
+
+	
+	MYST* sPtr = &s;
+	// sPtr + 1; // +1 하면 8바이트씩 계산
+	(*sPtr).a = 100; // sPtr->a = 100;
+}
---
+++ PersonalStudy/042강. 동적할당(2)/042강. 동적할당(2).cpp
@@ -0,0 +1,34 @@
+﻿// 042강. 동적할당(2)
+
+
+#include <iostream>
+void Test(int a)
+{
+	printf_s("%d\n", a);
+}
+int main()
+{
+	// 동적할당
+	// 1. 런타임 중에 대응 가능
+	// 2. 사용자가 직접 메모리를 관리해줘야함(해제)
+
+	Test(10);
+
+	int iInput = 0;
+	scanf_s("%d", &iInput);
+
+	int* iPtr = nullptr;
+	if (100 == iInput)
+	{
+		iPtr = (int*)malloc(100);
+	}
+
+	if (nullptr != iPtr)
+	{
+		// free함수는 내가 가리키는 주소의 
+		// "힙메모리를 해제하는 함수"
+		// "반드시 힙메모리의 주소를 넣어줘야함"
+		// iPtr이 해제되는건 아님
+		free(iPtr);
+	}
+}
